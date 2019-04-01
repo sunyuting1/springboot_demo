9b43d26ad493822600d484948795a0ad42da4b6a
Syt <15010922359@163.com>
2019-04-01 16:29:19 +0800

    默认的

---
+++ src/main/java/com/example/demo/HelloSpringBootController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+/**
+ * Created by My on 2019/4/1.
+ */
+@RestController
+public class HelloSpringBootController {
+    @RequestMapping("/hello")
+    public String hello() {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+        return list.get(0);
+    }
+}
